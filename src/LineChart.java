335de7362820ac53a9e8e29797a4a2f823289160
joaogabrieldosantosgoncalves@gmail.com <joaogabrieldosantosgoncalves@gmail.com>
2017-07-10 19:04:31 -0300

    fix chart range

--- src/LineChart.java
+++ src/LineChart.java
@@ -16,37 +16,56 @@ import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.NumberTickUnit;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
 
 import java.util.ArrayList;
 
 public class LineChart extends ApplicationFrame {
 
-    public LineChart(int startFrame, ArrayList<Integer> hintsFIFO, ArrayList<Integer> hintsMRU) {
+    public LineChart(int startFrame, ArrayList<Integer> hintsFIFO, ArrayList<Integer> hintsMRU, ArrayList<Integer> hintsSecondChance, ArrayList<Integer> hintsNUR) {
         super("Parent`s Job");
-        JFreeChart xylineChart = ChartFactory.createXYLineChart(
-                "Frames vs Hints" ,
-                "Hints" ,
-                "Frames" ,
-                (XYDataset) createDataset(startFrame, hintsFIFO, hintsMRU),
-                PlotOrientation.VERTICAL ,
-                true , true , false);
+
+        JFreeChart xylineChart = createPanel(startFrame, hintsFIFO, hintsMRU, hintsSecondChance, hintsNUR);
 
         ChartPanel chartPanel = new ChartPanel( xylineChart );
-        chartPanel.setPreferredSize( new java.awt.Dimension( 560 , 367 ) );
+        chartPanel.setPreferredSize( new java.awt.Dimension( 1200 , 700 ) );
         final XYPlot plot = xylineChart.getXYPlot( );
 
         XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer( );
         renderer.setSeriesPaint( 0 , Color.RED );
         renderer.setSeriesPaint( 1 , Color.GREEN );
         renderer.setSeriesPaint( 2 , Color.YELLOW );
+        renderer.setSeriesPaint( 3 , Color.BLUE );
         renderer.setSeriesStroke( 0 , new BasicStroke( 1.0f ) );
         renderer.setSeriesStroke( 1 , new BasicStroke( 1.0f ) );
         renderer.setSeriesStroke( 2 , new BasicStroke( 1.0f ) );
+        renderer.setSeriesStroke( 3 , new BasicStroke( 1.0f ) );
         plot.setRenderer( renderer );
         setContentPane( chartPanel );
     }
 
-    private XYDataset createDataset(int startFrame, ArrayList<Integer> hintsFIFO, ArrayList<Integer> hintsMRU) {
+    private JFreeChart createPanel(int startFrame, ArrayList<Integer> hintsFIFO, ArrayList<Integer> hintsMRU, ArrayList<Integer> hintsSecondChance, ArrayList<Integer> hintsNUR){
+        JFreeChart jfreechart = ChartFactory.createScatterPlot(
+                "Frames vs Hints", "Hints", "Frames", (XYDataset) createDataset(startFrame, hintsFIFO, hintsMRU, hintsSecondChance, hintsNUR),
+                PlotOrientation.VERTICAL, true, true, false);
+
+        XYPlot xyPlot = (XYPlot) jfreechart.getPlot();
+
+        NumberAxis domain = (NumberAxis) xyPlot.getDomainAxis();
+        domain.setRange(hintsFIFO.get(0), hintsMRU.get(hintsMRU.size() - 1));
+        domain.setTickUnit(new NumberTickUnit(500.0));
+        domain.setVerticalTickLabels(true);
+
+        NumberAxis range = (NumberAxis) xyPlot.getRangeAxis();
+        range.setRange(startFrame, hintsFIFO.size() + startFrame);
+        range.setTickUnit(new NumberTickUnit(1.0));
+
+        return  jfreechart;
+    }
+
+    private XYDataset createDataset(int startFrame, ArrayList<Integer> hintsFIFO, ArrayList<Integer> hintsMRU, ArrayList<Integer> hintsSecondChance, ArrayList<Integer> hintsNUR) {
 
             final XYSeries FIFO = new XYSeries( "FIFO" );
             double frames = (double) startFrame;
@@ -62,9 +81,26 @@ public class LineChart extends ApplicationFrame {
                 frames++;
             }
 
+            frames = (double) startFrame;
+            final XYSeries secondChance = new XYSeries( "Second Chance" );
+            for(Integer hint: hintsSecondChance) {
+                secondChance.add((double) hint, frames);
+                frames++;
+            }
+
+            frames = (double) startFrame;
+            final XYSeries NUR = new XYSeries( "NUR" );
+            for(Integer hint: hintsNUR) {
+                NUR.add((double) hint, frames);
+                frames++;
+            }
+
             final XYSeriesCollection dataset = new XYSeriesCollection( );
             dataset.addSeries( FIFO );
             dataset.addSeries( MRU );
+            dataset.addSeries( secondChance );
+            dataset.addSeries( NUR );
+
             return dataset;
     }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -3,12 +3,7 @@
  */
 
 import java.util.ArrayList;
-import java.util.Map;
 import java.util.Scanner;
-import org.jfree.chart.*;
-import org.jfree.data.*;
-import org.jfree.data.general.DefaultPieDataset;
-import org.jfree.data.general.PieDataset;
 import org.jfree.ui.RefineryUtilities;
 
 public class Menu {
@@ -16,6 +11,8 @@ public class Menu {
 
         ArrayList<Integer> hintsTotalFIFO = new ArrayList<>();
         ArrayList<Integer> hintsTotalMRU = new ArrayList<>();
+        ArrayList<Integer> hintsTotalSecondChance = new ArrayList<>();
+        ArrayList<Integer> hintsTotalNUR = new ArrayList<>();
 
         Scanner ler = new Scanner(System.in);
 
@@ -24,25 +21,30 @@ public class Menu {
 
         System.out.printf("\nConteúdo do arquivo texto:\n");
 
-        for(int i = 60; i<=75; i++) {
-            int hints = NUR.hint(File.readFile(nome), i, 500);
-            System.out.println("frame" + i + ": " + hints);
+//        for(int i = 60; i<=75; i++) {
+//            int hints = NUR.hint(File.readFile(nome), i, 500);
+//            System.out.println("frame" + i + ": " + hints);
+//        }
+
+        for(int i = 60; i<=75; i++){
+            System.out.print("frames" + String.valueOf(i) + ": ");
+
+            int hints = FIFO.hint(File.readFile(nome), i);
+            int hintsMRU = MRU.hint(File.readFile(nome), i);
+            int hintsSecondChance = SecondChance.hint(File.readFile(nome), i, 500);
+            int hintsNUR = NUR.hint(File.readFile(nome), i, 500);
+
+
+            System.out.println(hints);
+            hintsTotalFIFO.add(Integer.valueOf(hints));
+            hintsTotalMRU.add(Integer.valueOf(hintsMRU));
+            hintsTotalSecondChance.add(Integer.valueOf(hintsSecondChance));
+            hintsTotalNUR.add(Integer.valueOf(hintsNUR));
         }
 
-//        for(int i = 60; i<=75; i++){
-//            System.out.print("frames" + String.valueOf(i) + ": ");
-//
-//            int hints = FIFO.hint(File.readFile(nome), i);
-//            int hintsMRU = MRU.hint(File.readFile(nome), i);
-//
-//            System.out.println(hints);
-//            hintsTotalFIFO.add(Integer.valueOf(hints));
-//            hintsTotalMRU.add(Integer.valueOf(hintsMRU));
-//        }
-//
-//        LineChart chart = new LineChart(60, hintsTotalFIFO, hintsTotalMRU);
-//        chart.pack( );
-//        RefineryUtilities.centerFrameOnScreen( chart );
-//        chart.setVisible( true );
+        LineChart chart = new LineChart(60, hintsTotalFIFO, hintsTotalMRU, hintsTotalSecondChance, hintsTotalNUR);
+        chart.pack( );
+        RefineryUtilities.centerFrameOnScreen( chart );
+        chart.setVisible( true );
     }
 }
